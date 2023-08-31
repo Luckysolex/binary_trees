3fb6930b44ce99bca4e0e6710320a38b075d9745
Lucky <Luckysolex1@gmail.com>
2023-08-31 14:02:56 -0700

    file 0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,19 +9,18 @@
  *         Otherwise - a pointer to the new node
  *
  */
-
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new;
+	binary_tree_t *new_node;
 
-	new = malloc(sizeof(binary_tree_t));
-	if (new == NULL)
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+	{
 		return (NULL);
-
-	new->m = value;
-	new->parent = parent;
-	new->left = NULL;
-	new->right = NULL;
-
-	return (new);
+	}
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	return (new_node);
 }
