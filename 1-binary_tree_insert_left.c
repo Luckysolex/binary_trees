e8c4fc052d2c178ac651755895d6aba0ec9f1836
Lucky <Luckysolex1@gmail.com>
2023-08-31 14:09:38 -0700

    insert left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -18,11 +18,15 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *new;
 
 	if (parent == NULL)
+	{
 		return (NULL);
+	}
 
 	new = binary_tree_node(parent, value);
 	if (new == NULL)
+	{
 		return (NULL);
+	}
 
 	if (parent->left != NULL)
 	{
@@ -30,6 +34,5 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		parent->left->parent = new;
 	}
 	parent->left = new;
-
 	return (new);
 }
