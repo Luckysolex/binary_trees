54a908b97f2764200e85f76b540c7f42e97f6ad3
Lucky <Luckysolex1@gmail.com>
2023-08-31 15:49:19 -0700

    delete tree

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - Deletes an entire binary tree
+ * @tree: Pointer to the root of the node to delete
+ *
+ * Retrun: If tree is NULL, do nothing
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return;
+	else
+	{
+		if (tree != NULL)
+		{
+			binary_tree_delete(tree->left);
+			binary_tree_delete(tree->right);
+			free(tree);
+		}
+	}
+}
