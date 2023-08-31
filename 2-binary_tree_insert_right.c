a749d34c4eba7f2a6ed3c4a8587f6a2af012fe32
Lucky <Luckysolex1@gmail.com>
2023-08-31 15:35:40 -0700

    parentheses issues

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -2,27 +2,27 @@
 
 /**
  * binary_tree_insert_right - inserts a node as
- * 		the right-child of another node
+ *		the right-child of another node
  * @parent: Pointer to the node to insert
  * @value: Value of the new node inserted
  *
  * Return: pointer to the created node,
- * 	or NULL on failure or if parent is NULL
+ *	or NULL on failure or if parent is NULL
  *
  * Description: If parent already has a right-child,
- * 	the new node must take its place, and the old
- * 	right-child must be set as the right-child of the new node
+ *	the new node must take its place, and the old
+ *	right-child must be set as the right-child of the new node
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 
 	if (parent == NULL)
-		return NULL;
+		return (NULL);
 
 	new = binary_tree_node(parent, value);
 	if (new == NULL)
-		return NULL;
+		return (NULL);
 	if (parent->right != NULL)
 	{
 		new->right = parent->right;
