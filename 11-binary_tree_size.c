ae976ea85e987d37dab2c59de88d1be89bc553d5
Lucky <Luckysolex1@gmail.com>
2023-08-31 17:49:58 -0700

    size

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -8,17 +8,17 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t tree_size = 0, l = 0, r = 0;
+        size_t tree_size = 0, l = 0, r = 0;
 
-	if (tree == NULL)
-	{
-		return (0);
-	}
-	else
-	{
-		l = binary_tree_size(tree->left);
-		r = binary_tree_size(tree->right);
-		tre_size = l + r + 1;
-	}
-	return (tree_size);
+        if (tree == NULL)
+        {
+                return (0);
+        }
+        else
+        {
+                l = binary_tree_size(tree->left);
+                r = binary_tree_size(tree->right);
+                tree_size = l + r + 1;
+        }
+        return (tree_size);
 }
